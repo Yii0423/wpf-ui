8fe761800755943b8e7f12375f45ea56db413db4
yii0423 <939190181@qq.com>
2020-04-19 21:57:38 +0800

    button complete

---
+++ wpf-ui/Extends/DiyControls/DiyButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace wpf_ui.Extends.DiyControls
+{
+    /// <summary>
+    /// 自定义按钮
+    /// </summary>
+    public class DiyButton : Button
+    {
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(DiyButton), new PropertyMetadata(new CornerRadius(0)));
+
+    }
+}
