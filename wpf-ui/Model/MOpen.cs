0a9c5b6c1a635afb2cf87351a414f83cde95be65
zhangyi <zhangyi@mediinfo.com.cn>
2020-08-09 12:43:21 +0800

    replenish commit

---
+++ wpf-ui/Model/MOpen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace wpf_ui.Model
+{
+    /// <summary>
+    /// 窗体属性类
+    /// </summary>
+    public class MOpen
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 链接
+        /// </summary>
+        public Uri Url { get; set; }
+        /// <summary>
+        /// 宽度
+        /// </summary>
+        public double Width { get; set; } = 600;
+        /// <summary>
+        /// 高度
+        /// </summary>
+        public double Height { get; set; } = 400;
+        /// <summary>
+        /// 是否固定窗体大小
+        /// </summary>
+        public bool FixedSize { get; set; }
+        /// <summary>
+        /// 是否单击遮罩自动关闭
+        /// </summary>
+        public bool WhiteSpaceClose { get; set; }
+        /// <summary>
+        /// 是否打开即最大化
+        /// </summary>
+        public bool StartMax { get; set; }
+    }
+}
